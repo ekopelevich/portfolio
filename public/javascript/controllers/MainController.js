8eb7af1ce42aa329bc45d3b6013d8bdb83f1f83a
Elana Kopelevich <ekopelevich@gmail.com>
2016-02-13 12:09:59 -0700

    reorganization, content, fixed paths

--- public/javascript/controllers/MainController.js
+++ public/javascript/controllers/MainController.js
@@ -3,15 +3,6 @@ app.controller('MainController', function($scope){
   $scope.projects = [
     {
       thumbnail: '../../assets/planit-thumbnail.jpg',
-      name: 'Stumblr',
-      technologies: 'JavaScript, jQuery, Node, Express, Passport, MongoDB, Knex, Foundation',
-      description: 'Inspired, by the blog, "Wait but Why?", I created an interactive visualization of a life in weeks. This was a front-end focused project, created prior to learning about persistence, so events are just kept in localStorage, but the full-stack version is in the works.',
-      github: 'http://yourlifeinweeks.space',
-      liveURL: 'http://planit.space',
-      details: '#/projects/planit'
-    },
-    {
-      thumbnail: '../../assets/planit-thumbnail.jpg',
       name: 'Planit',
       technologies: 'JavaScript, jQuery, Node, Express, PostgreSQL, Knex, Passport, Bootstrap',
       description: 'Planit is a social event resource marketplace. You can post events and requests for help. You can also apply to other\' tasks based on your skillset as defined in your profile. This was a group project focused on demonstrating server-side proficiencies.',
@@ -22,11 +13,20 @@ app.controller('MainController', function($scope){
     {
       thumbnail: '../../assets/planit-thumbnail.jpg',
       name: 'Your Life in Weeks',
-      technologies: 'JavaScript, jQuery, localStorage',
-      description: 'I interactive visualization of a life in weeks. This was a front-end focused project, created prior to learning about persistence, so events are just kept in localStorage, but the full-stack version is in the works.',
+      technologies: 'JavaScript, jQuery, Bootstrap, localStorage',
+      description: 'An interactive visualization of a life in weeks. This was a front-end focused project, created prior to learning about persistence, so life events are kept in localStorage, but the full-stack version is in the works.',
       github: 'http://yourlifeinweeks.space',
       liveURL: 'http://planit.space',
-      details: '#/projects/planit'
+      details: '#/projects/weeks'
+    // },
+    // {
+    //   thumbnail: '../../assets/planit-thumbnail.jpg',
+    //   name: 'Stumblr',
+    //   technologies: 'JavaScript, jQuery, localStorage',
+    //   description: 'I interactive visualization of a life in weeks. This was a front-end focused project, created prior to learning about persistence, so events are just kept in localStorage, but the full-stack version is in the works.',
+    //   github: 'http://yourlifeinweeks.space',
+    //   liveURL: 'http://planit.space',
+    //   details: '#/projects/planit'
     }
   ];
 })
